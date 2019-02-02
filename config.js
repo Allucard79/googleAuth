42dd49fc6ca3a2ba7da754658cc77218995cb2fc
Marcin Ratajczak <maratajczak@gmail.com>
2019-02-02 15:42:18 +0100

    Init project

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '14929278444-nc04037fq3csl8lrpk6k0phk2i0aj0vu.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'G4OMI6AzNB67K2vc07FL_pZ7',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
